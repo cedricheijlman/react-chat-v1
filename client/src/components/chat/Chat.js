4edb8033e4737c073dc0cb0af5ccd18a0465dd6b
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-02-15 19:36:33 +0100

    chat room

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -2,7 +2,74 @@ import React from "react";
 import "./chat.css";
 
 function Chat() {
-  return <div>Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatBox">
+        <div>
+          <h2>Room Name</h2>
+        </div>
+        <div className="messages">
+          <div className="message">
+            <div className="messageUser">
+              <h5>Usernamedddddd</h5>
+              <h6>23:22</h6>
+            </div>
+            <p>
+              This his is a messagehis is a messagehis is a messagehis is a mes
+              sagehis is a messagehis is a mes sagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              shis is a messagessagehis is a messagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              messagehis is a message messageis a message
+            </p>
+          </div>
+
+          <div className="message">
+            <div className="messageUser">
+              <h5>Username</h5>
+              <h6>23:22</h6>
+            </div>
+            <p>
+              This his is a messagehis is a messagehis is a messagehis is a mes
+              sagehis is a messagehis is a mes sagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              shis is a messagessagehis is a messagehis is a messagehis is a
+              messagehis is a messagehis is a messagehis is a messagehis is a
+              messagehis is a message messageis a message
+            </p>
+          </div>
+
+          <div className="message">
+            <div className="messageUser">
+              <h5>Username</h5>
+              <h6>23:22</h6>
+            </div>
+            <p>
+              This his is a messagehis is a messagehis is a messagehis is a mes
+            </p>
+          </div>
+        </div>
+
+        <div className="messageBox">
+          <input
+            className="messageInput"
+            placeholder="Send message to Room Name"
+          />
+          <button>Send Message</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
